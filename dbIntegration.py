cd7eb35bb10b9cb9c380b4168b4145d8730064f2
cabrex01 <71939834+cabrex01@users.noreply.github.com>
2022-04-05 16:14:10 +0530

    Updated dbIntegration.py

--- dbIntegration.py
+++ dbIntegration.py
@@ -22,7 +22,7 @@ else:
 	cluster = MongoClient(mongodbclient_token, tlsCAFile=ca)
 
 
-db = cluster["maverix"]
+db = cluster["ticket_scanner"]
 participants = db["participants"]
 
 print("Database connection has been established\n")
@@ -54,6 +54,7 @@ def delete_participants_details(hash):
 
     participants.delete_one({"hash": str(hash)})
     return details
+
 
 def shell():
     while(True):
@@ -98,25 +99,83 @@ def shell():
 
 
         elif broken_argument[0].lower() in ["drop"]:
-            while(True):
-                final_check = input("Are you sure you want to drop the table 'participants'?: ")
-                if final_check.lower() in ["y", "yes"]:
-                    participants.drop()
-                    print("'participants' table has been dropped")
-                    break
+            if len(broken_argument) > 1 and broken_argument[1].lower() != "":
+                if broken_argument[1].lower() in db.list_collection_names():
 
-                elif final_check.lower() in ["n", "no"]:
-                    print("Table has not been dropped")
-                    break
+                    while(True):
+                        final_check = input(f"Are you sure you want to drop the table '{broken_argument[1].lower()}'?: ")
+                        if final_check.lower() in ["y", "yes"]:
+                            db[broken_argument[1].lower()].drop()
+                            print(f"'{broken_argument[1].lower()}' table has been dropped")
+                            break
+
+                        elif final_check.lower() in ["n", "no"]:
+                            print("Table has not been dropped")
+                            break
+                        else:
+                            print("Please enter y or n")
+
                 else:
-                    print("Please enter y or n")
+                    print(f"The table '{broken_argument[1].lower()}' does not exist")
+            else:
+                table_to_delete = input("Enter the name of the table to drop: ")
+                if table_to_delete.lower() in db.list_collection_names():
+                    while(True):
+                        final_check = input(f"Are you sure you want to drop the table '{table_to_delete.lower()}'?: ")
+                        if final_check.lower() in ["y", "yes"]:
+                            db[table_to_delete.lower()].drop()
+                            print(f"'{table_to_delete.lower()}' table has been dropped")
+                            break
+
+                        elif final_check.lower() in ["n", "no"]:
+                            print("Table has not been dropped")
+                            break
+                        else:
+                            print("Please enter y or n")
+                else:
+                    print(f"table '{table_to_delete}' doesnt exist")
 
 
         elif broken_argument[0].lower() == 'exit':
             print("bye")
             return
+
+        elif (broken_argument[0].lower() == "check"):
+
+            if len(broken_argument) > 1 and broken_argument[1].lower() != "":
+                if broken_argument[1].lower() in db.list_collection_names():
+                    print(f"'{broken_argument[1].lower()}' table exists")
+                else:
+                    print(f"'{broken_argument[1].lower()}' table doesnt exists")
+            
+            else:
+                table_input = input("Enter the table name to check ('Enter' to print all the tables): ")
+                if table_input.strip() == "":
+                    print("The table name(s) are:")
+                    for table in db.list_collection_names():
+                        print(table)
+                else:
+                    if table_input in db.list_collection_names():
+                        print(f"'{table_input}' table exists")
+                    else:
+                        print(f"'{table_input}' table doesnt exist")
+
+        elif (broken_argument[0].lower() in ["clear", "cls"]):
+            if os.name == 'nt':
+                os.system("cls")
+            else:
+                os.system("clear")
+            print("Database shell")
+
+        elif (broken_argument[0].lower() in ["ls", "la"]):
+            if os.name == 'nt':
+                os.system("dir")
+            else:
+                os.system("ls -la")
+
+
         else:
-            print(f"No command found {broken_argument[0]}")
+            print(f"'{broken_argument[0]}': command not found")
 
 
 if __name__ == "__main__": 
